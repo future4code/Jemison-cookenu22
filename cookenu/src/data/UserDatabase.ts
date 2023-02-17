5dc4eb4cde302cda6bf6dbe40765f5203c18d5c1
rafix923 <rafaellopes.dev@gmail.com>
2023-02-17 00:27:27 -0300

    feat: Transforma a função insertUser em um método dentro da classe UserDatabase.

---
+++ cookenu/src/data/UserDatabase.ts
@@ -0,0 +1,18 @@
+import { BaseDatabase } from "./BaseDatabase";
+import { user } from "../types/user"
+
+export class UserDatabase extends BaseDatabase {
+
+    insertUser = async (user: user): Promise<void> => {
+        try {
+            await UserDatabase.connection.insert({
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                password: user.password
+            }).into('Architecture_User')
+        } catch (error: any) {
+            throw new Error(error.message)
+        }
+    }
+}
