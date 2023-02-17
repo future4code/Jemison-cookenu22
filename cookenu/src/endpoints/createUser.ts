08e145f193ee4ac22d001782e332ac9cc9899a9d
rafix923 <rafaellopes.dev@gmail.com>
2023-02-16 23:29:39 -0300

    chore(template): Troca o template para arquitetura de software I.
    Essa alteração me permitirá realizar a atividade revisando o conteúdo da aula de arquitetura de software I.

---
+++ cookenu/src/endpoints/createUser.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { insertUser } from "../data/insertUser";
+
+
+export const createUser = async (
+   req: Request,
+   res: Response
+) => {
+   try {
+      const { name, email, password } = req.body
+
+      if (
+         !name ||
+         !email ||
+         !password
+      ) {
+         throw new Error('Preencha os campos "name","nickname", "email" e "password"')
+      }
+
+      const id: string = Date.now().toString()
+
+      await insertUser({
+         id,
+         name,
+         email,
+         password
+      })
+
+      res.status(201).send({ message: "Usuário criado!" })
+   } catch (error:any) {
+      res.status(400).send(error.message)
+   }
+}
