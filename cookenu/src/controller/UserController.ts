0b1dc1638cc4923eb7fe36658e9af280211b2054
rafix923 <rafaellopes.dev@gmail.com>
2023-02-17 01:36:56 -0300

    feat: Cria o método createUser no arquivo UserController.ts.

--- cookenu/src/controller/UserController.ts
+++ cookenu/src/controller/UserController.ts
@@ -1,3 +1,18 @@
-export class UserController{
-    
+import { Request, Response } from "express";
+
+export class UserController {
+
+    createUser = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const input: any = {
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            };
+
+            res.status(201).send({ message: "Usuário criado!" })
+        } catch (error: any) {
+            res.status(400).send(error.message)
+        }
+    }
 }
--- cookenu/src/endpoints/createUser.ts
+++ cookenu/src/endpoints/createUser.ts
@@ -1,32 +1,11 @@
 import { Request, Response } from "express";
 
 
-export const createUser = async (
-   req: Request,
-   res: Response
-) => {
+export const createUser = async (req: Request, res: Response) => {
    try {
-      const { name, email, password } = req.body
-
-      if (
-         !name ||
-         !email ||
-         !password
-      ) {
-         throw new Error('Preencha os campos "name","email" e "password"')
-      }
-
-      const id: string = Date.now().toString()
-
-      await insertUser({
-         id,
-         name,
-         email,
-         password
-      })
 
       res.status(201).send({ message: "Usuário criado!" })
-   } catch (error:any) {
+   } catch (error: any) {
       res.status(400).send(error.message)
    }
 }
