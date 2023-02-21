d0d2b51974a31d71ffac8e0a2d6ee6ad26b7ec3b
rafix923 <rafaellopes.dev@gmail.com>
2023-02-20 21:26:56 -0300

    refactore(index.ts): Apaga a instância criada no arquivo index.ts.
    O instanciamento agora faz parte do arquivo userRouter.ts.

--- cookenu/src/index.ts
+++ cookenu/src/index.ts
@@ -1,12 +1,9 @@
 import { app } from "./app";
-import { UserController } from "./controller/UserController";
 import { userRouter } from "./routes/userRouter";
-
-const userController = new UserController();
 
 
 // Rota para o user
-app.use("/signup", userRouter)
+app.use("/user", userRouter)
 
 
 
