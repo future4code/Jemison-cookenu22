b2dfce77cc2f63324e40f0baeb507b24deb792da
rafix923 <rafaellopes.dev@gmail.com>
2023-02-21 22:41:16 -0300

    refactore: Adiciona recipe como segunda parte do path para criação de receita.

--- cookenu/src/index.ts
+++ cookenu/src/index.ts
@@ -1,11 +1,11 @@
 import { app } from "./app";
 import { userRouter } from "./routes/userRouter";
-
+import { recipeRouter } from "./routes/recipeRouter";
 
 // Rota para o user
 app.use("/", userRouter)
 
 // Rota para o recipe
-app.use("/crate", userRouter)
+app.use("/create", recipeRouter)
 
 
