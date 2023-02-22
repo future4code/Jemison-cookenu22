a61ded3ce5ebd7cff7fcbc8186422144063830ec
rafix923 <rafaellopes.dev@gmail.com>
2023-02-22 20:34:02 -0300

    feat(UserDTO): Cria a interface InsertUserInputDTO.

--- cookenu/src/model/userDTO.ts
+++ cookenu/src/model/userDTO.ts
@@ -3,3 +3,10 @@ export interface UserInputDTO {
     email: string,
     password: string
 }
+
+export interface InsertUserInputDTO {
+    id:string
+    name: string,
+    email: string,
+    password: string
+}
