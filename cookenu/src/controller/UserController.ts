94894d3410d9b3c09ebfc793bf3125b1397190f5
rafix923 <rafaellopes.dev@gmail.com>
2023-02-22 20:29:28 -0300

    feat(UserDTO): Importa tipage da interface userInputDTO para o input do UserController.

--- cookenu/src/controller/UserController.ts
+++ cookenu/src/controller/UserController.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
 import { UserBusiness } from "../business/UserBusiness";
+import { UserInputDTO } from "../model/userDTO";
 
 export class UserController {
 
     createUser = async (req: Request, res: Response): Promise<void> => {
         try {
-            const input: any = {
+            const input: UserInputDTO = {
                 name: req.body.name,
                 email: req.body.email,
                 password: req.body.password
