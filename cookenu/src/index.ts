92106612608f3e48ff7dd5d13d455e561f2113e5
rafix923 <rafaellopes.dev@gmail.com>
2023-02-21 21:55:01 -0300

    feat(controller): Cria a rota para a criação de receitas.

--- cookenu/src/index.ts
+++ cookenu/src/index.ts
@@ -5,5 +5,7 @@ import { userRouter } from "./routes/userRouter";
 // Rota para o user
 app.use("/", userRouter)
 
+// Rota para o recipe
+app.use("/crate", userRouter)
 
 
---
+++ cookenu/src/routes/recipeRouter.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { RecipeController } from "../controller/RecipeController";
+
+export const recipeRouter = express.Router();
+
+const recipeController = new RecipeController()
+
+recipeRouter.post('/recipe', recipeController.createRecipe)
